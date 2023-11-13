fa04f9a9911e6da67cfbd1e1f3aee1c892e4ca55
CocoSebadi <116943179+CocoSebadi@users.noreply.github.com>
2023-11-13 12:53:44 +0200

    Update sharepoint.py

--- sharepoint.py
+++ sharepoint.py
@@ -1,49 +1,58 @@
 import streamlit as st
-import sqlite3
 
-# Create a SQLite database connection
-conn = sqlite3.connect('project_issues.db')
-c = conn.cursor()
+def login():
+    # Placeholder authentication logic
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-# Create a table to store issues
-c.execute('''
-          CREATE TABLE IF NOT EXISTS issues (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              rating INTEGER,
-              overdue_issues TEXT,
-              due_date DATE,
-              risk_type TEXT
-          )
-          ''')
-conn.commit()
+    if username == "demo" and password == "password":
+        return True
+    else:
+        return False
 
-# Streamlit App
 def main():
     st.title("Project Issues Form")
 
-    # Login Section (Dummy Authentication)
-    if st.button("Login"):
-        st.success("Login Successful!")
-        show_form()
+    if login():
+        st.success("Login successful!")
 
-def show_form():
-    # Project Issues Form
-    st.header("Project Issues Form")
+        # Show a pop-up message for choosing to log a new issue or update an existing issue
+        issue_action = st.radio("Select an action:", ["Log a New Issue", "Update an Existing Issue"])
 
-    rating = st.selectbox("Rating:", [1, 2, 3, 4, 5])
-    overdue_issues = st.text_input("Overdue Issues:")
-    due_date = st.date_input("Due Date:")
-    risk_type = st.selectbox("Risk Type:", ["Low", "Medium", "High"])
+        st.header("Issue Details")
 
-    if st.button("Submit"):
-        # Save the issue to the database
-        save_issue(rating, overdue_issues, due_date, risk_type)
-        st.success("Issue submitted successfully!")
+        issue_name = st.text_input("Issue Name")
+        issue_description = st.text_area("Issue Description")
 
-def save_issue(rating, overdue_issues, due_date, risk_type):
-    c.execute("INSERT INTO issues (rating, overdue_issues, due_date, risk_type) VALUES (?, ?, ?, ?)",
-              (rating, overdue_issues, due_date, risk_type))
-    conn.commit()
+        risk_type = st.selectbox("Risk Type", ["Low", "Medium", "High"])
+
+        entities = st.multiselect("Entities", ["Entity A", "Entity B", "Entity C"])
+
+        causal_category = st.selectbox("Causal Category", ["Category 1", "Category 2", "Category 3"])
+
+        bu_rating = st.slider("BU Rating", min_value=1, max_value=5)
+
+        agile_rating = st.slider("Agile Rating", min_value=1, max_value=5)
+
+        assurance_provider = st.text_input("Assurance Provider")
+
+        due_date = st.date_input("Due Date")
+
+        # If updating an existing issue, ask for additional evidence
+        if issue_action == "Update an Existing Issue":
+            st.header("Update Evidence")
+            update_evidence = st.text_area("Provide written evidence for the update")
+
+            # Option to attach a file
+            uploaded_file = st.file_uploader("Attach a File (if any)", type=["pdf", "docx", "xlsx"])
+
+        # Add a button to submit the form
+        if st.button("Submit"):
+            # Add logic to handle form submission, storing the data, etc.
+            st.success("Form submitted successfully!")
+
+    else:
+        st.error("Login failed. Please try again.")
 
 if __name__ == "__main__":
     main()
